08a6994b517ab2260a38c5f4ed25cea32fdfcd18
NehaDesai123 <37369276+NehaDesai123@users.noreply.github.com>
2025-11-18 21:46:41 +0530

    Adjust profile image height and standardize base path configuration in Vite setup

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/portfolio-neha_desai/' : '/',
+  base: '/portfolio-neha_desai/',
   css: {
     postcss: './postcss.config.js'
   },
